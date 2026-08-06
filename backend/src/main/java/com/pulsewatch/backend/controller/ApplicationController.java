7ad33d4663e7935deb450633676f46bbe3b3548d
PradnyaJadhav289 <pradnyaj289@gmail.com>
2026-08-06 12:48:54 +0530

    feat(application): implement complete CRUD REST API using Spring Data JPA

--- backend/src/main/java/com/pulsewatch/backend/controller/ApplicationController.java
+++ backend/src/main/java/com/pulsewatch/backend/controller/ApplicationController.java
@@ -2,7 +2,12 @@ package com.pulsewatch.backend.controller;
 
 import com.pulsewatch.backend.entity.Application;
 import com.pulsewatch.backend.service.ApplicationService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+//Annotations start with:@
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/applications")
@@ -17,6 +22,25 @@ public class ApplicationController {
     @PostMapping
     public Application createApplication(@RequestBody Application application) {
         return applicationService.saveApplication(application);
+    }
+    @GetMapping
+    public List<Application> getAllApplications() {
+        return applicationService.getAllApplications();
+    }
+    @GetMapping("/{id}")
+    public Optional<Application> getApplicationById(@PathVariable Long id) {
+        return applicationService.getApplicationById(id);
+    }
+    @PutMapping("/{id}")
+    public Application updateApplication(@PathVariable Long id,
+                                         @RequestBody Application application) {
+
+        return applicationService.updateApplication(id, application);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteApplication(@PathVariable Long id) {
+        applicationService.deleteApplication(id);
+        return ResponseEntity.ok("Application deleted successfully.");
     }
 
 }
--- backend/src/main/java/com/pulsewatch/backend/service/ApplicationService.java
+++ backend/src/main/java/com/pulsewatch/backend/service/ApplicationService.java
@@ -3,6 +3,8 @@ package com.pulsewatch.backend.service;
 import com.pulsewatch.backend.repository.ApplicationRepository;
 import org.springframework.stereotype.Service;
 import com.pulsewatch.backend.entity.Application;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ApplicationService {
@@ -15,5 +17,29 @@ public class ApplicationService {
     public Application saveApplication(Application application) {
         return applicationRepository.save(application);
     }
+    public List<Application> getAllApplications() {
+        return applicationRepository.findAll();
+    }
+    public Optional<Application> getApplicationById(Long id) {
+        return applicationRepository.findById(id);
+    }
+    public Application updateApplication(Long id, Application updatedApplication) {
+
+        Application existingApplication = applicationRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Application not found"));
+
+        existingApplication.setApplicationName(updatedApplication.getApplicationName());
+        existingApplication.setOwnerTeam(updatedApplication.getOwnerTeam());
+        existingApplication.setEnvironment(updatedApplication.getEnvironment());
+
+        return applicationRepository.save(existingApplication);
+    }
+    public void deleteApplication(Long id) {
+        if (!applicationRepository.existsById(id)) {
+            throw new RuntimeException("Application not found");
+        }
+        applicationRepository.deleteById(id);
+    }
+
 
 }
