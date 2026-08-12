4d633fea9d6cce16c264dfd76d77a34dc49387f0
PradnyaJadhav289 <pradnyaj289@gmail.com>
2026-08-12 14:18:08 +0530

    day12 : read side of the API Metrics module

--- backend/src/main/java/com/pulsewatch/backend/repository/ApiMetricRepository.java
+++ backend/src/main/java/com/pulsewatch/backend/repository/ApiMetricRepository.java
@@ -2,7 +2,14 @@ package com.pulsewatch.backend.repository;
 
 import com.pulsewatch.backend.entity.ApiMetric;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface ApiMetricRepository
         extends JpaRepository<ApiMetric, Long> {
+
+    Page<ApiMetric> findByApplicationId(
+            Long applicationId,
+            Pageable pageable
+    );
 }
